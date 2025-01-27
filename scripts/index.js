8219866fd7cb5eb7e900bce48465f23e139faa21
Neekolay <sedmirvk@mail.ru>
2025-01-27 20:06:25 +0300

    нихера не понял, но очень интересно

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,15 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const container = document.querySelector('.places');
+const placesList = container.querySelector('.places__list');
+const deleteButton = container.querySelector('.card__delete-button');
+
+initialCards.forEach(addCard);
+
+function addCard() {
+    const placeTemplate = document.querySelector('#card-template').content;
+    const placeElements = placeTemplate.querySelectorAll('.card').cloneNode(true);
+
+} 
